3e73bb990684f523f23da38c7bd0c31d2a4c0603
Justin Waulters <justin@tapgnomes.com>
2022-04-13 21:15:33 -0400

    feat(config) added config variables

--- box-clicker.js
+++ box-clicker.js
@@ -1,5 +1,19 @@
+// the starting integer of the game start countdown timer
 var countdown_from = 2;
-var countdown_interval;
+
+// percent size of the box relative to the game area in %
+var default_box_size_x = 10;
+var default_box_size_y = 10;
+
+// the farthest the the top-left corner can go, in %, before the box touches an edge
+var max_x = 100 - default_box_size_x;
+var max_y = 100 - default_box_size_y;
+
+// seconds before next box location for the first transition.
+var initial_game_speed = 3;
+
+// how fast the game speeds each time in %
+var faster_increment = 3;
 
 function start_game() {
     $( "#box" ).show();
