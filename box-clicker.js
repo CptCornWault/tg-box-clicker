3a97e7ed28b3be92bb43ef9f935dcf0ac4461102
Justin Waulters <justin@tapgnomes.com>
2022-04-16 23:59:31 -0400

    feat(game script) first working version

--- box-clicker.js
+++ box-clicker.js
@@ -9,29 +9,70 @@ var default_box_size_y = 10;
 var max_x = 100 - default_box_size_x;
 var max_y = 100 - default_box_size_y;
 
-// seconds before next box location for the first transition.
-var initial_game_speed = 3;
+// milliseconds before next box location for the first transition.
+var initial_game_speed = 2000;
 
-// how fast the game speeds each time in %
-var faster_increment = 3;
+// how fast the game speeds each time in % (expressed as a decimal)
+var faster_increment = 0.03;
+
+function show_box() {
+    $( "#box" ).show();
+}
+
+function hide_box() {
+    $( "#box" ).hide();
+}
+
+function position_box() {
+    x_position = Math.floor( Math.random() * max_x );
+    y_position = Math.floor( Math.random() * max_y );
+    $( "#box" ).css( 'top', x_position + "%" );
+    $( "#box" ).css( 'left', y_position + "%" );
+}
+
+function box_wait() {
+    console.log( current_game_speed );
+    end_game_timeout = setTimeout( () => { end_game() }, current_game_speed );
+}
+
+function end_game() {
+    $( "#box" ).hide();
+    $( "#too_slow" ).show();
+    $( "#start" ).show();
+}
 
 function start_game() {
-    $( "#box" ).show();
+    show_box();
+    box_wait();
 }
 
 function countdown() {
     $( "#countdown" ).show();
-
-    if( countdown_from == 0 ){
+    if( current_countdown == 0 ){
         clearInterval( countdown_interval );
         $( "#countdown" ).hide()
         start_game();
     }
-    
-    $( "#countdown" ).text( countdown_from );
-    countdown_from -= 1;
+    $( "#countdown" ).text( current_countdown );
+    current_countdown -= 1;
 }
 
 $( "#start" ).click( function() {
+    // console.log( "click" );
+    current_countdown = countdown_from;
+    current_game_speed = initial_game_speed
+    $( "#too_slow" ).hide();
+    $( "#start" ).hide();
+    position_box();
+    countdown();
     countdown_interval = setInterval( countdown, 1000 );
+} );
+
+$( "#box" ).click( function() {
+    clearTimeout( end_game_timeout );
+    hide_box();
+    position_box();
+    current_game_speed = Math.floor( current_game_speed - current_game_speed * faster_increment );
+    show_box();
+    box_wait();
 } );
